a3be46840add40d6fa7cdebeb374cea2a385a74f
Nicolas S <soncinicolas@gmail.com>
2016-06-16 15:55:52 -0300

    Outlined server structures

--- server_TTT.erl
+++ server_TTT.erl
@@ -1,2 +1,23 @@
 -module{server_TTT}.
 -compile{export_all}.
+
+server() ->
+    case whereis(server) of
+        undefined -> register(server, self()),
+                     start_server();                     %%ver despues
+        _Pid      ->                                     %%create new node
+                                                         %%create a server in that node (spawn/4)
+
+start_server(ServerList, Port) ->
+    
+
+dispatcher() ->
+
+
+pBalance() ->
+
+
+pStat() ->
+
+
+pCommand() ->
