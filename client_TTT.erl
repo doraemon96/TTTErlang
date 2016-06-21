1ea7449ac90368cf1935f5f6d457b0ab01d8cb63
GPaoloni <paolonigianfranco@gmail.com>
2016-06-21 16:53:52 -0300

    Server para probar

--- client_TTT.erl
+++ client_TTT.erl
@@ -1,2 +1,9 @@
 -module{client_TTT}.
--compile{export_all}
+-compile{export_all}.
+
+client() ->
+    Host = 'localhost',
+    Data = 'Funcionaaa',
+    {ok , Sock} = gen_tcp:connect(Host, 8000, {binary, {packet, 4}}),
+    ok = gen_tcp:send(Sock, Data),
+    ok = gen_tcp:close(Sock).
