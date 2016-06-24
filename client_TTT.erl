0d7c33489afb5578973cbf230b539e3966c07e3d
Nicolas S <soncininicolas@gmail.com>
2016-06-24 12:17:12 -0300

    Preguntar por disponibilidad de usuario en conexion

--- client_TTT.erl
+++ client_TTT.erl
@@ -3,7 +3,28 @@
 
 client() ->
     Host = "localhost",
-    Data = "Funcionaaa",
     {ok , Sock} = gen_tcp:connect(Host, 8000, [binary, {packet, 4}]),
+    case username_loop(Sock) of
+        {username, UserName} -> ok;
+        _ -> {error, not_supported}
+    end,
+    client_loop(Sock).
+
+username_loop(Sock) ->
+    io:format("Ingrese nombre de usuario: "),
+    UserName = string:strip(io:get_line(""), right, $\n),
+    ok = gen_tcp:send(Sock, "CON "++UserName),
+    receive
+        {tcp, Sock, valid_username} -> 
+            {username, UserName};
+        {tcp, Sock, invalid_username} ->
+            io:format("Usuario en uso.~n"),
+            username_loop(Sock).
+        _ -> {error, not_supported}
+    end.
+
+client_loop(Sock) -> 
+    Data = string:strip(io:get_line("Comando:"), right, $\n),
     ok = gen_tcp:send(Sock, Data),
-    ok = gen_tcp:close(Sock).
+    %ok = gen_tcp:close(Sock).
+    client_loop(Sock).
