740eb38997b81d23f6a0e3b061ad6e7612331509
Nicolas S <soncininicolas@gmail.com>
2016-08-22 15:02:35 -0300

    Commented changes

--- commands_TTT.erl
+++ commands_TTT.erl
@@ -1,6 +1,10 @@
 -module(commands_TTT).
 -compile(export_all).
 
-cmd_connect(UserName) ->
-    case query_PList(UserName) of
-        true -> {}
+%%cmd_connect(UserName) ->
+%%    case query_PList(UserName) of
+%%        true -> {}
+try_connect(Username) ->
+    %%Probamos si el usuario no existe
+    Global = global:registered_names(),
+    case lists:any(fun(X) -> X =:= Username, Global) of
